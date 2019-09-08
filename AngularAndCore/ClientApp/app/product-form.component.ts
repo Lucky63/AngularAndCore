6e763a0ae3a2142d6ce928e18573ad7d91ccc2dd
Lucky63 <liceist2008@mail.ru>
2019-09-08 17:38:55 +0600

    update3

--- AngularAndCore/ClientApp/app/product-form.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component, Input } from '@angular/core';
-import { Product } from './product';
-@Component({
-	selector: "product-form",
-	templateUrl: './product-form.component.html'
-})
-export class ProductFormComponent {
-	@Input() product: Product;
-}
--- AngularAndCore/wwwroot/dist/polyfills.js
+++ AngularAndCore/wwwroot/dist/polyfills.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7bd7ee803165b80385d7";
+/******/ 	var hotCurrentHash = "f3c799b7228fc35e60f9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
